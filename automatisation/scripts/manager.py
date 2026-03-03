e7eabe4a81954f7ad8bc9be5ce76961ef9611f29
keylian15 <keylian59555@gmail.com>
2026-03-03 10:37:53 +0100

    fix(force)

--- automatisation/scripts/manager.py
+++ automatisation/scripts/manager.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
+import sys
+
+force = "--force" in sys.argv or "-f" in sys.argv
 
 # Verifier les deprecated warnings et erreurs dans les logs de compilation, et generer un rapport structuré en markdown
 from verif_deprecated import DeprecatedAnalyzer
-#DeprecatedAnalyzer().run()
+# DeprecatedAnalyzer().run()
 
 # Verifier et mettre à jour les README des projets dans borne_arcade/projet
 from verify_and_update_readme import ReadmeManager
-ReadmeManager(force_update=False).run()
+ReadmeManager(force_update=force).run()
 
 # Verifier et mettre à jour les docstrings des fichiers sources de chaque projet
 from verify_and_update_docs import DocsManager
-DocsManager(force_update=False).run()
+DocsManager(force_update=force).run()
 
 # Generer et mettre à jour la documentation utilisateur globale de la borne arcade
 from verify_and_update_doc_user import UserDocManager
-UserDocManager(force_update=False).run()
+UserDocManager(force_update=force).run()

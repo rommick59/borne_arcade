196b8906bce89de9536508983faec1702b8158d2
keylian15 <keylian59555@gmail.com>
2026-03-05 09:51:39 +0100

    docs lancer borne

--- automatisation/hooks/setup-hooks.sh
+++ automatisation/hooks/setup-hooks.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+print_section() {
+    echo ""
+    echo "=================================================="
+    echo " $1"
+    echo "=================================================="
+}
 
-echo "Installation des hooks..."
+
+print_section "Installation des hooks..."
 
 cp automatisation/hooks/post-merge .git/hooks/post-merge
 chmod +x .git/hooks/post-merge
-
-echo "Hooks installés."

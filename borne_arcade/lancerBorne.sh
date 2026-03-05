b749039288c88fc579837d664ad8d6b7b71703d2
keylian15 <keylian59555@gmail.com>
2026-03-05 16:47:40 +0100

    fix(script): passage des copies de lancer a install

--- borne_arcade/lancerBorne.sh
+++ borne_arcade/lancerBorne.sh
@@ -7,16 +7,6 @@ print_section() {
     echo "=================================================="
 }
 
-# === 0. Copier le fichier de configuration clavier ===
-print_section "Setup des touches de la borne"
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-sudo cp $SCRIPT_DIR/borne /usr/share/X11/xkb/symbols/borne
-
-# === 0.5. Setup lancement automatique
-print_section "Setup du lancement automatique"
-mkdir -p $HOME/.config/autostart
-sudo cp $SCRIPT_DIR/borne.desktop $HOME/.config/autostart
-
 setxkbmap borne
 
 print_section "Nettoyage des répertoires. Veuillez patienter"

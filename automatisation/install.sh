dba827dd7931214bd7f878dc80967be1dbf2f3d4
keylian15 <keylian59555@gmail.com>
2026-03-06 13:18:15 +0100

    fix(install): fix files paths

--- automatisation/install.sh
+++ automatisation/install.sh
@@ -180,12 +180,12 @@ main() {
     # === 0. Copier le fichier de configuration clavier ===
     print_section "Setup des touches de la borne"
     SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-    sudo cp $SCRIPT_DIR/borne /usr/share/X11/xkb/symbols/borne
+    sudo cp $SCRIPT_DIR/../borne_arcade/borne /usr/share/X11/xkb/symbols/borne
 
     # === 0.5. Setup lancement automatique
     print_section "Setup du lancement automatique"
     mkdir -p $HOME/.config/autostart
-    sudo cp $SCRIPT_DIR/borne.desktop $HOME/.config/autostart
+    sudo cp $SCRIPT_DIR/../borne_arcade/borne.desktop $HOME/.config/autostart
 
     SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
